6b532a33363f1f9eff167c20cacc749549f62f9a
Tuğsem Saranlı <saranlitugsem@gmail.com>
2023-12-20 22:59:35 +0300

    update lockfile

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+bundle lock --add-platform x86_64-linux
+bundle exec rake app:update:bin
 bundle install
 ./bin/rails assets:precompile
 ./bin/rails rake assets:clean
