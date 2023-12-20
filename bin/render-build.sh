9f3e60c1f67265d411d1adb674ca4d38cbf34166
Tuğsem Saranlı <saranlitugsem@gmail.com>
2023-12-21 01:01:03 +0300

    remove the line

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,6 @@
 # exit on error
 set -o errexit
 
-bundle lock --add-platform x86_64-linux
-bundle exec rake app:update:bin
 bundle install
 ./bin/rails assets:precompile
 ./bin/rails rake assets:clean
