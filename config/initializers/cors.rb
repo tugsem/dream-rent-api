ba3f7caef5e81e3c2b2f7f383411eed9d6ba2664
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-01-03 17:41:45 +0300

    Fix error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,13 +9,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000"
             "http://127.0.0.1:3000",
-            "https://dream-rent-irrw.onrender.com"
+            "https://dream-rent-irrw.onrender.com",
 
     resource '*',
-    headers: :any,
-    methods: %i[get post put patch delete options head],
-    credentials: true,
-    max_age: 86400
+      headers: :any,
+      methods: %i[get post put patch delete options head],
+      credentials: true,
+      max_age: 86400
   end
-
 end
