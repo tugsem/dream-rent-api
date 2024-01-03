1052d70756f09f4610c9f3be4f3370a6c6c03113
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-01-03 17:23:02 +0300

    Create controller for cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # mount Rswag::Ui::Engine => '/api-docs'
   # mount Rswag::Api::Engine => '/api-docs'
+  options '*path', to: 'application#cors_preflight_check'
+
   namespace :api do
     namespace :v1 do
       post 'users/login'
