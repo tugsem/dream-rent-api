3dbcd17a77d6348dac4dbda293c3d9d6fe743b5c
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-01-11 17:28:12 +0300

    Add route for active storage

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   # mount Rswag::Ui::Engine => '/api-docs'
   # mount Rswag::Api::Engine => '/api-docs'
+  direct :blob do |model, options|
+    blob = model.blob
+    route_for(:rails_blob, blob.signed_id, blob.filename, only_path: true)
+  end
+
   namespace :api do
     namespace :v1 do
-      post 'users/login'
-      post 'users/register'
-      get 'users/index'
+      post 'users/login', to: 'users/login'
+      post 'users/register', to: 'users/register'
+      get 'users/index', to: 'users/index'
 
       resources :appointments
       resources :houses
