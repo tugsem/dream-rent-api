ef177ca49d72f0f10e9b7f54e0d3263e64b2c728
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-01-29 23:34:21 +0300

    change cors origin for local development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000",
-            "http://127.0.0.1:3000",
+    origins "http://localhost:3001",
+            "http://127.0.0.1:3001",
             "https://dream-rent-irrw.onrender.com"
 
     resource '*',
